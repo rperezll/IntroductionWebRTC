c51cd4d560f5261c0bc422b0f5795006c8b984dd
WINROB\Winrob <robertoprz898@gmail.com>
2017-07-05 15:44:40 +0200

    Snapshot included

--- StreamCam/script.js
+++ StreamCam/script.js
@@ -1,20 +1,41 @@
-(function (){
+$(document).ready(function (){
+    
+    var width = 400;
+    var height = 300;
+    var localMediaStream = null;
     var video = document.getElementById('cam'), 
         vendorURL = window.URL || window.webKitURL;
     navigator.getMedia = navigator.getUserMedia || 
                         navigator.webkitUserMedia ||
                         navigator.mozGetUserMedia ||
                         navigator.msGetUserMedia;
-    
+
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+
+    $('#snapshot').click( function(){
+        //console.log("funciona");
+		if (localMediaStream!=null) {
+            canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+            document.querySelector('img').src = canvas.toDataURL('image/png');
+        }
+	});
+
+    $('#grabar').click( function(stream){
+        
+        
+	});
+
     navigator.getMedia({
         video:true,
         audio: false
     }, function(remoteStream){
         video.src = window.URL.createObjectURL( remoteStream );
+        localMediaStream = remoteStream;
         video.play();
-        //video.src = vendorURL.createObjectURL(stream);
-        //video.play();
     }, function(error){
         //Code error
     });
-})();
+
+});
